16d0ada24b5837820db58151181546d73aa00c1b
Luke Bond (1000139488) <BONDLI1@student.op.ac.nz>
2026-06-11 13:52:58 +1200

    changes

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+namespace mathcalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            Console.WriteLine("Math Calculator");
+            Console.WriteLine("You can select what type of calculation\nyou would like to do by pressing the number next to it");
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Binary();
+                    break;
+                case 2:
+                    Geometryvector();
+                    break;
+                case 3:
+                    Matrices();
+                    break;
+                case 4:
+                    Numbertheory();
+                    break;
+                case 5:
+                    Crypto();
+                    break;
+                default:
+                    Console.WriteLine("Wrong entry");
+                    break;
+
+            }
+        }
+        public static void Binary()
+        {
+
+        }
+        public static void Geometryvector()
+        {
+
+        }
+        public static void Matrices()
+        {
+
+        }
+        public static void Numbertheory()
+        {
+
+        }
+        public static void Crypto()
+        {
+
+        }
+
+    }
+}
